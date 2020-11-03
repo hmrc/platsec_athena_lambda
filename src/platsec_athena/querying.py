0be97d08c16edb22c040155b9df97aede33643f5
Mark Teasdale <mteasdal@btinternet.com>
2020-11-03 09:49:20 +0000

    Updated tests

--- src/platsec_athena/querying.py
+++ src/platsec_athena/querying.py
@@ -1,5 +1,38 @@
-class Statement :
-    def __init__(self,config):
-        self.config = config
-        self.output = "s3://"+self.config.output+"/DailyAthenaLogs/CloudTrail/"+self.config.eventstamp
+from abc import ABC, abstractmethod
+from enum import Enum
+from platsec_athena.config import StatementType
+
+class QueryType(Enum):
+    AUDIT = 1
+
+
+class Query(ABC):
+    def __init__(self,config,query_type):
+      self._query_type = query_type.value
+
+    @property
+    @abstractmethod
+    def get_query_type(self):
+        return self._query_type
+
+    @abstractmethod
+    def get_query_text(self,config):
+        pass
+
+class AuditQuery(Query):
+    def get_query_text(self,config):
+        date_clause = query_dates["date_clause"]
+        name = "k"
+        age = 12
+        query_text = f"Hello, {name}. You are {age}."
+
+        return query_text
+
+class QueryFactory:
+    def get_query_type(self,query_type):
+        queries = {
+            1: AuditQuery
+            }
+
+        return queries.get(query_type, "Invalid Query Type")
 
--- tests/unit/test_platsec_athena.py
+++ tests/unit/test_platsec_athena.py
@@ -2,31 +2,31 @@ import pytest
 import datetime
 import time
 from platsec_athena.config import LambdaEnvironment
-from platsec_athena.querying import Statement
+from platsec_athena.querying import AuditQuery, QueryType, QueryFactory
 from platsec_athena.config import StatementType
 from platsec_athena.partioning import Partition
 
 @pytest.mark.config
-def test_s3_bucket_location_must_be_correctly_formatted() :
-   config = get_config()
-   expected_output = "s3://"+config.output+"/DailyAthenaLogs/CloudTrail/"+config.eventstamp
-
-   statement = get_statement(config)
-
-   assert statement.output == expected_output
-
-@pytest.mark.config
 def test_query_dates_and_statements_must_be_correctly_formatted():
-   config = get_config()
-   expected_date_clause =f'year=\'{config.athena_year}\' AND month=\'{config.athena_month}\' AND day=\'{config.athena_day}\';'
+    '''
+    Test that query statements are
+    configured correctly
+    '''
+    config = get_config()
+    expected_date_clause =f'year=\'{config.athena_year}\' AND month=\'{config.athena_month}\' AND day=\'{config.athena_day}\';'
 
 
-   statements = config.get_querydates()
-   actual_date_clause = statements['date_clause']
-   assert actual_date_clause == expected_date_clause
+    statements = config.get_querydates()
+    actual_date_clause = statements['date_clause']
+    assert actual_date_clause == expected_date_clause
 
 @pytest.mark.config
 def test_config_statements_must_be_correctly_formatted():
+    '''
+       Test that config statements
+       for Athena Database and Tables
+       are formatted correctly
+    '''
     config = get_config()
     expected_statement  =f'{config.db}.{config.table}'
 
@@ -37,6 +37,10 @@ def test_config_statements_must_be_correctly_formatted():
 
 @pytest.mark.config
 def test_partition_statements_must_be_returned_for_each_region_specified():
+    '''
+       Tests that the number of partition statements
+       are correctly set up
+    '''
     config = get_config()
     expected_statement_count = 2
 
@@ -45,20 +49,28 @@ def test_partition_statements_must_be_returned_for_each_region_specified():
 
 @pytest.mark.config
 def test_partition_statements_must_be_correctly_formatted():
-    config = get_config()
-    statement_count = 2
+    '''
+      Tests the partitions statements are
+      correctly formatted
+    '''
+    sut = get_config()
     expected_statements = []
-    for i in range(len(config.regions)):
-        statement_to_test = [f'partition (region="{config.regions[i]}", month="{config.athena_month}", day="{config.athena_day}", year="{config.athena_year}")']
+    for i in range(len(sut.regions)):
+        statement_to_test = \
+                f'partition (region="{sut.regions[i]}", month="{sut.athena_month}", day="{sut.athena_day}", year="{sut.athena_year}")'
         expected_statements.append(statement_to_test)
 
-    statements = config.get_partitions()
+    statements = sut.get_partitions()
 
     for expected_statement in expected_statements:
-        assert statements_check(expected_statement,expected_statements) == True
+        assert statements_check(expected_statement,statements) == True
 
 @pytest.mark.config
 def test_partition_statements_will_error_with_no_regions_specified():
+    '''
+        Tests that an error is returned
+        when no regions are specified
+    '''
     config = get_config(regions=[])
     with pytest.raises(IndexError) as excinfo:
         statements = config.get_partitions()
@@ -67,6 +79,10 @@ def test_partition_statements_will_error_with_no_regions_specified():
 
 @pytest.mark.config
 def test_location_statements_count_must_equal_region_count():
+    '''
+    Tests that region count and the number of
+    Location statements are the same
+    '''
     config = get_config()
 
     statements = config.get_locations()
@@ -75,6 +91,11 @@ def test_location_statements_count_must_equal_region_count():
 
 @pytest.mark.config
 def test_location_statements_will_error_with_no_regions_specified():
+    '''
+    Test that an error is returned 
+    when the region number and the
+    returned location count dont match
+    '''
     config = get_config(regions=[])
     with pytest.raises(IndexError) as excinfo:
         statements = config.get_locations()
@@ -83,6 +104,10 @@ def test_location_statements_will_error_with_no_regions_specified():
 
 @pytest.mark.config
 def test_location_statements_must_be_correctly_formatted():
+    '''
+    Tests that the locations statements
+    are formatted correctly
+    '''
     config = get_config()
     statement_count = 2
     expected_statements = []
@@ -97,24 +122,36 @@ def test_location_statements_must_be_correctly_formatted():
 
 @pytest.mark.config
 def test_select_statement_must_be_correctly_formatted():
-    config = get_config()
-    expected_statement = f'SELECT * FROM {config.table} WHERE '
+    '''
+    Tests that a selection statement
+    is returned for all specified regions
+    '''
+    sut = get_config()
+    expected_statement = f'SELECT * FROM {sut.table} WHERE '
 
-    statement = config.get_statements(StatementType.SELECT)
+    statement = sut.get_statements(StatementType.SELECT)
 
     assert expected_statement == statement
 
 @pytest.mark.config
 def test_location_statement_must_be_correctly_formatted():
-    config = get_config()
-    expected_statement = f' \'%{config.bucket}%\''
+    '''
+    Tests the location statements
+    are formatted correctly
+    '''
+    sut = get_config()
+    expected_statement = f' \'%{sut.bucket}%\''
 
-    statement = config.get_statements(StatementType.BUCKET)
+    statement = sut.get_statements(StatementType.BUCKET)
 
     assert expected_statement == statement
 
-@pytest.mark.partion
+@pytest.mark.partition
 def test_partioning_query_must_be_formatted_correctly():
+    '''
+    Tests that the partitioning query is correctly 
+    formatted
+    '''
     config = get_config()
     partition = get_partition(config)
     config_partitions = config.get_partitions()
@@ -125,7 +162,36 @@ def test_partioning_query_must_be_formatted_correctly():
 
     assert partition_query == expected_partition_query
 
+@pytest.mark.query
+@pytest.mark.parametrize('qt',[QueryType.AUDIT])
+def test_query_factory_returns_specified_query_type(qt):
+    '''
+    Tests that the query factory
+    returns the correct query
+    '''
+    sut = get_query_factory()
+    query = sut.get_query_type(qt.value)
+    expected_query_type = qt.value
+
+    actual_query_type = query.get_query_type
+
+    assert actual_query_type ==  expected_query_type
+
+
+#@pytest.mark.pciquery
+#def test_pci_audit_trail_query_must_be_formatted_correctly():
+#    config = get_config()
+#    query_dates = config.get_querydates()
+#    expected_audit_query = f'{config.get_statements(StatementType.SELECT)} requestparameters LIKE {config.get_statements(StatementType.BUCKET)} AND sourceipaddress <> \'cloudtrail.amazonaws.com\' AND sourceipaddress <> \'athena.amazonaws.com\' AND eventName = \'GetObject\' AND {query_dates["date_clause"]}'
+#    athena_query = get_query(config)
+
+#    assert audit_query == expected_audit_query
+
 def get_config(db="test_db",table="test_table",bucket="test_bucket",output="test_output",account="test_account",regions=["eu-west-1","eu-west-2"]):
+    '''
+    Returns a LambdaEnvironment config
+    object to allow for testing within tests
+    '''
     test_date = str(datetime.datetime.today().isoformat())
     test_config = LambdaEnvironment(db,table,bucket,output,account,test_date,regions)
     return test_config
@@ -134,12 +200,19 @@ def get_partition(config):
     test_partition = Partition(config)
     return test_partition
 
-def get_statement(config):
-    test_statement = Statement(config)
-
-    return test_statement
+def get_query_factory():
+    '''
+    Returns a query factory object 
+    to be used in tests
+    '''
+    query_factory = QueryFactory()
+    return query_factory
 
 def statements_check(item, items):
+    '''
+    Helper function for checking items
+    in a collection
+    '''
     if item in items:
         return True
     else:
